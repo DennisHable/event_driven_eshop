84b1e0c8e2ed5156f0026fbc5f960101157ede66
DennisHable <dennishable@seznam.cz>
2026-09-26 00:49:52 +0200

    add new product service impl

--- backend/src/Service/AdminService.php
+++ backend/src/Service/AdminService.php
@@ -12,5 +12,31 @@ class AdminService {
     public function __construct(private readonly EntityManagerInterface $entityManager) {}
 
 
+    public function addNewProduct(array $data, ?User $user): array {
+        if(!$user || !in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+            return ['success' => false, 'error' => 'Přidání produktu může provádět jen přihlášený ADMIN.', 'code' => 400];
+        }
+
+        // musí být přítomny: "name, price, stock, category, description"
+        if(!isset($data['name']) || !isset($data['price']) || !isset($data['stock']) || !isset($data['category']) || !isset($data['description'])) { // pokud v poli $data neexistují ty klíče nebo existují ale jsou null
+            return ['success' => false, 'error' => 'Chybějící povinné parametry produktu.', 'code' => 400];
+        }
+
+        $product = new Product();
+        $product->setName($data['name'])
+            ->setPrice($data['price'])
+            ->setStock($data['stock'])
+            ->setCategory($data['category'])
+            ->setDescription($data['description']);
+
+        // $productRepo = $this->entityManager->getRepository(Product::class);
+
+        $this->entityManager->persist($product); // předání instance Produktu do Doctrine ORM
+        $this->entityManager->flush(); // zapsání do DB
+
+        return ['success' => true, 'productId' => $product->getId(), 'code' => 201];
+    }
+
+
 
 }
