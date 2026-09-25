fe79bf342ab5be9a079d06a2141e87c31d908de1
DennisHable <dennishable@seznam.cz>
2026-09-26 00:48:45 +0200

    admin service

---
+++ backend/src/Service/AdminService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Product;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AdminService {
+
+
+}
