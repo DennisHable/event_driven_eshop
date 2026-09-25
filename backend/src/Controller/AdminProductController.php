3166ebaf1b01f221f38f629124b09aef2a50496d
DennisHable <dennishable@seznam.cz>
2026-09-26 00:42:02 +0200

    AdminController

---
+++ backend/src/Controller/AdminProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Service\AdminService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AdminProductController extends AbstractController {
+
+}
